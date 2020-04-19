715c3d5110be2687706e7d4ecaad7518374519a3
ryo-rm <6457344+ryo-rm@users.noreply.github.com>
2020-04-19 16:48:05 +0900

    ブラウザを使ったレンダリング方法を作成

---
+++ web_img/index.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+
+const { argv } = require('yargs')
+  .usage('Usage: $0 [options]')
+  .example('$0 -u http://example.com', 'render HTML from webpage')
+  .example('$0 -u /path/to.file', 'render HTML from file')
+  .options({
+    'url': {
+      alias: 'u',
+      describe: 'render URL',
+      demandOption: true
+    },
+    'width': {
+      alias: 'x',
+      describe: 'width',
+      default: 300,
+      type: 'number'
+    },
+    'height': {
+      alias: 'y',
+      describe: 'height',
+      default: 400,
+      type: 'number'
+    },
+    'scale': {
+      describe: 'viwport scale',
+      default: 1,
+      type: 'number'
+    },
+    'path': {
+      alias: 'p',
+      describe: 'save path',
+      default: '/tmp/webimg.png'
+    },
+    'sp': {
+      describe: 'emulate smartphone',
+      default: false,
+      type: 'boolean'
+    },
+    'timeout': {
+      describe: 'browser navigation timeout(ms)',
+      default: 3000000,
+      type: 'number'
+    }
+  });
+
+const puppeteer = require('puppeteer');
+const { exec } = require('child_process');
+
+(async () => {
+  try {
+
+    const width = argv.x * argv.scale;
+    const height = argv.y * argv.scale;
+
+    const browser = await puppeteer.launch({
+      product: 'chrome',
+      executablePath: '/usr/bin/chromium-browser',
+      headless: true
+    });
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(argv.timeout);
+    if (argv.sp) {
+      // iPhone X/Chrome
+      const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/77.0.3865.69 Mobile/15E148 Safari/605.1';
+      await page.emulate({
+        viewport: {
+          width,
+          height,
+          isMobile: true
+        },
+        userAgent: UA
+      });
+    } else {
+      await page.setViewport({ width, height });
+    }
+    await page.goto(argv.u);
+    await page.screenshot({path: argv.p});
+  
+    browser.close();
+
+    const { stdout, stderr } = exec(`${process.cwd()}/../show_img/show_img.py ${argv.p}`);
+    //console.log(stdout);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
+
